c569b2a66750375d1924acb40e6a88fb262a0d79
akzo <anarkazimov98@hotmail.com>
2016-06-28 00:12:26 +0300

    Edited our front end

--- app_scr/www/index.php
+++ app_scr/www/index.php
@@ -3,9 +3,43 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width">
-        <title>Blank App</title>
+        <title>IChange</title>
     </head>
     <body>
         <script type="text/javascript" src="cordova.js"></script>
+        
+        
+        
+        <!--table for making-->
+        <table style="height:100%; " bordercolor = "black" width="100%"  background-color="purple">
+            
+        <!--upper row-->
+        <tr  style="background-color:#990066">
+        <td width=10% height=90> 
+            <!-- dashed button -->
+            <center>
+            <button>
+            <img src="dash_button.svg" width=40 height=55 alt="">
+            </button>
+            </center>       
+        </td>  
+            
+        <td>
+            <center>
+                <font size="+5" face="times">ICHANGE</font>    
+            </center>
+        </td>
+        </tr>
+            
+        <!--bottom row-->
+        <tr>
+            <td colspan = 2 >
+            
+            <iframe width = 100% height=100% src = "">
+            </td>
+        </tr>
+            
+            
+        </table>
     </body>
 </html>
