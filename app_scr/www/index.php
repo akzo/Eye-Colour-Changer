f361a700b778b9ce044c04d1510fdc59e267d4f4
akzo <anarkazimov98@hotmail.com>
2016-06-28 22:13:14 +0300

    updated our index.php

--- app_scr/www/index.php
+++ app_scr/www/index.php
@@ -4,6 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width">
         <title>IChange</title>
+        
+        <!-- some fancy styles-->
+        <style>
+        .btn {
+            background-color: #990066; /* purple */
+            }
+        </style>
+        
+        
     </head>
     <body>
         <script type="text/javascript" src="cordova.js"></script>
@@ -11,20 +20,21 @@
         
         
         <!--table for making-->
-        <table style="height:100%; " bordercolor = "black" width="100%"  background-color="purple">
+        <table style="height:100%; bordercolor=black; width=100% ; background-color=purple;
+        position: absolute; top: 0; bottom: 0; left: 0; right: 0;">
             
         <!--upper row-->
-        <tr  style="background-color:#990066">
-        <td width=10% height=90> 
+        <tr  style="background-color:#990066; height: 15%; width=100%">
+        <td> 
             <!-- dashed button -->
             <center>
-            <button>
+            <button class="btn">
             <img src="dash_button.svg" width=40 height=55 alt="">
             </button>
             </center>       
         </td>  
             
-        <td>
+        <td width=100%>
             <center>
                 <font size="+5" face="times">ICHANGE</font>    
             </center>
@@ -32,7 +42,7 @@
         </tr>
             
         <!--bottom row-->
-        <tr>
+        <tr  style="height: 75%; width=100%">
             <td colspan = 2 >
             
             <iframe width = 100% height=100% src = "">
